f2f51473f7cdc9275504b4422097a08612e8cb4c
swoimszlakiem-sklep <143903787+swoimszlakiem-sklep@users.noreply.github.com>
2024-01-14 12:32:48 +0100

    create github pages

---
+++ assets/viewportchecker/viewportchecker.js
@@ -0,0 +1,3 @@
+
+window.addEventListener("load",function(){var f=function(d,c,b,e){function a(){var a=(new Date).getTime(),a=Math.max((f-a)/e,0),a=Math.round(b-a*g);d.innerHTML=a;a==b&&clearInterval(h)}var g=b-c;c=Math.abs(Math.floor(e/g));c=Math.max(c,50);var f=(new Date).getTime()+e,h;h=setInterval(a,c);a()},k=new IntersectionObserver(function(d,c){d.forEach(function(b){0<b.intersectionRatio&&b.target.querySelectorAll("#"+b.target.id+" .count").forEach(function(b){var a=b.innerText.trim(),a=2<a.length-(a.indexOf(".")+
+1)?a.replace(/\./ig,""):Math.floor(a);f(b,0,a,3E3)})})},{rootMargin:"0px",threshold:.1});document.querySelectorAll(".counters").forEach(function(d){k.observe(d)})});
